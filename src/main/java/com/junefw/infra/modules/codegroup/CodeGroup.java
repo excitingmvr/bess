18362538cd921525b51df6cf7b877fff0a386150
excitingmvr <excitingmvr@gmail.com>
2022-08-31 17:31:45 +0900

    zsdfgasdf

---
+++ src/main/java/com/junefw/infra/modules/codegroup/CodeGroup.java
@@ -0,0 +1,192 @@
+package com.junefw.infra.modules.codegroup;
+
+import java.util.Date;
+
+public class CodeGroup {
+	
+//	infr_code_group
+	private String ifcgSeq;
+	private String ifcgSeqAnother;
+	private String ifcgName;
+	private String ifcgNameEng;
+	private Integer ifcgUseNy;
+	private Integer ifcgOrder;
+	private String ifcgReferenceV1;
+	private String ifcgReferenceV2;
+	private String ifcgReferenceV3;
+	private Integer ifcgReferenceI1;
+	private Integer ifcgReferenceI2;
+	private Integer ifcgReferenceI3;
+	private String ifcgDesc;
+	private Integer ifcgDelNy;
+	private String regIp;
+	private String regSeq;
+	private String regId;
+	private Integer regDeviceCd;
+	private Date regDateTime;
+	private Date regDateTimeSvr;
+	private String modIp;
+	private String modSeq;
+	private String modId;
+	private Integer modDeviceCd;
+	private Date modDateTime;
+	private Date modDateTimeSvr;
+//----------
+	public String getIfcgSeq() {
+		return ifcgSeq;
+	}
+	public void setIfcgSeq(String ifcgSeq) {
+		this.ifcgSeq = ifcgSeq;
+	}
+	public String getIfcgSeqAnother() {
+		return ifcgSeqAnother;
+	}
+	public void setIfcgSeqAnother(String ifcgSeqAnother) {
+		this.ifcgSeqAnother = ifcgSeqAnother;
+	}
+	public String getIfcgName() {
+		return ifcgName;
+	}
+	public void setIfcgName(String ifcgName) {
+		this.ifcgName = ifcgName;
+	}
+	public String getIfcgNameEng() {
+		return ifcgNameEng;
+	}
+	public void setIfcgNameEng(String ifcgNameEng) {
+		this.ifcgNameEng = ifcgNameEng;
+	}
+	public Integer getIfcgUseNy() {
+		return ifcgUseNy;
+	}
+	public void setIfcgUseNy(Integer ifcgUseNy) {
+		this.ifcgUseNy = ifcgUseNy;
+	}
+	public Integer getIfcgOrder() {
+		return ifcgOrder;
+	}
+	public void setIfcgOrder(Integer ifcgOrder) {
+		this.ifcgOrder = ifcgOrder;
+	}
+	public String getIfcgReferenceV1() {
+		return ifcgReferenceV1;
+	}
+	public void setIfcgReferenceV1(String ifcgReferenceV1) {
+		this.ifcgReferenceV1 = ifcgReferenceV1;
+	}
+	public String getIfcgReferenceV2() {
+		return ifcgReferenceV2;
+	}
+	public void setIfcgReferenceV2(String ifcgReferenceV2) {
+		this.ifcgReferenceV2 = ifcgReferenceV2;
+	}
+	public String getIfcgReferenceV3() {
+		return ifcgReferenceV3;
+	}
+	public void setIfcgReferenceV3(String ifcgReferenceV3) {
+		this.ifcgReferenceV3 = ifcgReferenceV3;
+	}
+	public Integer getIfcgReferenceI1() {
+		return ifcgReferenceI1;
+	}
+	public void setIfcgReferenceI1(Integer ifcgReferenceI1) {
+		this.ifcgReferenceI1 = ifcgReferenceI1;
+	}
+	public Integer getIfcgReferenceI2() {
+		return ifcgReferenceI2;
+	}
+	public void setIfcgReferenceI2(Integer ifcgReferenceI2) {
+		this.ifcgReferenceI2 = ifcgReferenceI2;
+	}
+	public Integer getIfcgReferenceI3() {
+		return ifcgReferenceI3;
+	}
+	public void setIfcgReferenceI3(Integer ifcgReferenceI3) {
+		this.ifcgReferenceI3 = ifcgReferenceI3;
+	}
+	public String getIfcgDesc() {
+		return ifcgDesc;
+	}
+	public void setIfcgDesc(String ifcgDesc) {
+		this.ifcgDesc = ifcgDesc;
+	}
+	public Integer getIfcgDelNy() {
+		return ifcgDelNy;
+	}
+	public void setIfcgDelNy(Integer ifcgDelNy) {
+		this.ifcgDelNy = ifcgDelNy;
+	}
+	public String getRegIp() {
+		return regIp;
+	}
+	public void setRegIp(String regIp) {
+		this.regIp = regIp;
+	}
+	public String getRegSeq() {
+		return regSeq;
+	}
+	public void setRegSeq(String regSeq) {
+		this.regSeq = regSeq;
+	}
+	public String getRegId() {
+		return regId;
+	}
+	public void setRegId(String regId) {
+		this.regId = regId;
+	}
+	public Integer getRegDeviceCd() {
+		return regDeviceCd;
+	}
+	public void setRegDeviceCd(Integer regDeviceCd) {
+		this.regDeviceCd = regDeviceCd;
+	}
+	public Date getRegDateTime() {
+		return regDateTime;
+	}
+	public void setRegDateTime(Date regDateTime) {
+		this.regDateTime = regDateTime;
+	}
+	public Date getRegDateTimeSvr() {
+		return regDateTimeSvr;
+	}
+	public void setRegDateTimeSvr(Date regDateTimeSvr) {
+		this.regDateTimeSvr = regDateTimeSvr;
+	}
+	public String getModIp() {
+		return modIp;
+	}
+	public void setModIp(String modIp) {
+		this.modIp = modIp;
+	}
+	public String getModSeq() {
+		return modSeq;
+	}
+	public void setModSeq(String modSeq) {
+		this.modSeq = modSeq;
+	}
+	public String getModId() {
+		return modId;
+	}
+	public void setModId(String modId) {
+		this.modId = modId;
+	}
+	public Integer getModDeviceCd() {
+		return modDeviceCd;
+	}
+	public void setModDeviceCd(Integer modDeviceCd) {
+		this.modDeviceCd = modDeviceCd;
+	}
+	public Date getModDateTime() {
+		return modDateTime;
+	}
+	public void setModDateTime(Date modDateTime) {
+		this.modDateTime = modDateTime;
+	}
+	public Date getModDateTimeSvr() {
+		return modDateTimeSvr;
+	}
+	public void setModDateTimeSvr(Date modDateTimeSvr) {
+		this.modDateTimeSvr = modDateTimeSvr;
+	}
+
+}
